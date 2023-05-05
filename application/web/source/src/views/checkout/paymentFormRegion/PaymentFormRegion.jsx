e1b93af03f2347fd613aba6d872d594108b59f14
tldav <tldavis09@yahoo.com>
2023-05-05 12:12:56 -0500

    app(payment form region): added state for currency and source

--- application/web/source/src/views/checkout/paymentFormRegion/PaymentFormRegion.jsx
+++ application/web/source/src/views/checkout/paymentFormRegion/PaymentFormRegion.jsx
@@ -1,16 +1,43 @@
-import { useState } from "react"
+import { useState } from "react";
 
 function PaymentFormRegion() {
-  return (
-      <form id="payment-form-region">
-        <label htmlFor="currency">Currency</label>
-        <input id="currency" name="currency" error-id="currency-error" onBlur={e => validateInput(e.target)}></input>
-        <p id="currency-error" className="hide-error error">Invalid Currency</p>
-        <label htmlFor="source">Source</label>
-        <input id="source" name="source" error-id="source-error" onBlur={e => validateInput(e.target)}></input>
-        <p id="source-error" className="hide-error error">Invalid Source</p>
-      </form>
-  )
+	const [source, setSource] = useState("");
+	const [currency, setCurrency] = useState("");
+
+	const handleCurrencyChange = (e) => {
+		setCurrency(e.target.value);
+	};
+
+	const handleSourceChange = (e) => {
+		setSource(e.target.value);
+	};
+
+	const validateInput = () => {};
+
+	return (
+		<form id="payment-form-region">
+			<label htmlFor="currency">Currency</label>
+			<input
+				id="currency"
+				name="currency"
+				onBlur={validateInput}
+				onChange={handleCurrencyChange}
+			></input>
+			<p id="currency-error" className="hide-error error">
+				Invalid Currency
+			</p>
+			<label htmlFor="source">Source</label>
+			<input
+				id="source"
+				name="source"
+				onBlur={validateInput}
+				onChange={handleSourceChange}
+			></input>
+			<p id="source-error" className="hide-error error">
+				Invalid Source
+			</p>
+		</form>
+	);
 }
 
-export default PaymentFormRegion
+export default PaymentFormRegion;
