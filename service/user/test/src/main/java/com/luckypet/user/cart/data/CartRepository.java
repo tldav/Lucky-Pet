0d3decb770618883abac02371b0149b7ef1dbb71
tldav <tldavis09@yahoo.com>
2023-05-23 12:15:04 -0500

    Refactor(user service): cleaned up CartRepository

--- service/user/test/src/main/java/com/luckypet/user/cart/data/CartRepository.java
+++ service/user/test/src/main/java/com/luckypet/user/cart/data/CartRepository.java
@@ -2,7 +2,6 @@ package com.luckypet.user.cart.data;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -16,6 +15,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CartRepository {
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/lucky_pet_db";
+    public static final String MYSQL_USER = Environment.getInstance().getProperty("user");
+    public static final String MYSQL_PASSWORD = Environment.getInstance().getProperty("password");
+    
     public static CartRepository getInstance() {
         return new CartRepository();
     }
@@ -24,34 +27,28 @@ public class CartRepository {
         return null;
     }
     
+    @SuppressWarnings({"UnusedReturnValue", "unchecked"})
     public List<Cart> getCartsFromMySQLDB() {
+        List<Cart> carts = new ArrayList<>();
         try {
             DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/lucky_pet_db", Environment.getInstance().getProperty("user"), Environment.getInstance().getProperty("password"));
+            Connection connection = DriverManager.getConnection(DB_URL, MYSQL_USER, MYSQL_PASSWORD);
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM _cart");
-            JSONObject jsonObject = new JSONObject();
             JSONArray jsonArray = new JSONArray();
-            int columnCount = resultSet.getMetaData().getColumnCount();
             while (resultSet.next()) {
                 JSONObject record = new JSONObject();
                 record.put("id", resultSet.getInt("id"));
                 record.put("username", resultSet.getString("username"));
                 jsonArray.add(record);
             }
-            jsonObject.put("cart data", jsonArray);
-
-            List<Cart> carts;
             Gson gson = new GsonBuilder().setPrettyPrinting().create();
             carts = gson.fromJson(jsonArray.toJSONString(), new TypeToken<List<Cart>>() {
             }.getType());
-            System.out.println(gson.toJson(carts));
             connection.close();
         } catch (Exception e) {
             System.out.println(e);
         }
-
-        
-        return null;
+        return carts;
     }
 }
