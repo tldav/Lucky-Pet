1df211294a616ad39d4405be50b93b601a2b03f5
tldav <tldavis09@yahoo.com>
2023-06-14 11:45:38 -0500

    IT(web app): fixed cart data issue and updated data seed

--- service/user/source/src/main/resources/data_seed.sql
+++ service/user/source/src/main/resources/data_seed.sql
@@ -37,8 +37,8 @@ VALUES("Sample text", 500, 230),
 ("Sample description", 99999999, 0),
 ("Sample description", 50, -1),
 (NULL, 500, 30),
-("Sample description", 49, 50),
+("Sample description", 0, 50),
 ("Sample description", 49, 50);
 
 INSERT INTO _item (quantity, cart_id, product_id)
-VALUES (5,1,1), (5,2,2), (5,3,3), (5,4,4), (5,5,5), (5,6,6), (5,7,7), (5,8,11), (5,8,8), (5,9,10), (5,10,10);
+VALUES (5,1,1), (5,2,2), (5,3,3), (5,4,4), (5,5,5), (5,6,6), (5,7,7), (5,8,11), (5,8,8), (5,9,10), (5,10,9);
